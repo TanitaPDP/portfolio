81bec0728a0a49a5aea0e9165431dba97629d161
TanitaPDP <tanita.pdp@gmail.com>
2019-09-04 08:39:27 +0300

    Base markup of portfolio site

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// Файл gulpfile.js для работы в папке src
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+// Add style from sass or scss to style.min.css, clean code, compress code
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({            
+            prefix: "",
+            suffix: ".min"            
+          }))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Watch for changes in sass or scss and html files made - browser will reload
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+// Include all tasks in one default
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
